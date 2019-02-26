9c89171ef1653f044b785a707ad5cd225ad95b5e
jessejamesblack <jessejamesblack@optonline.net>
2019-02-26 09:14:56 -0500

    some components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/Forms';
+
 
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from './components/my-component/my-component.component';
-import {UserComponent} from './components/user/user.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent,
-    UserComponent
+    UserComponent,
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,12 +1,32 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-user',
-  templateUrl: './user-component.component.html',
-  styleUrls: ['./user-component.component.css']
-  // template: '<h2>John Doe</h2>'
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
 })
 
-export class UserComponent {
+export class UserComponent implements OnInit {
+  // Properties
+  user: User;
 
+  // Methods
+  constructor() {
+    
+  } 
+
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main st',
+        city: 'Boston',
+        state: 'MA'
+      }
+    }
+  }
 }
