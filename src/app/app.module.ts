5f34df328432cba3e645165d0f9d04ab96e5c2e7
jessejamesblack <jessejamesblack@optonline.net>
2019-02-27 09:38:05 -0500

    overhaul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/Forms';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { LogService } from './services/log.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    UsersComponent,
-    NavbarComponent
+    NavbarComponent,
+    LogFormComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
